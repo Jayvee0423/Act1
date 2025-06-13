0fae91f66567145419f303121ddc00c6734a5424
John Vincent A. Modelo <jayvee@Jayvees-MacBook-Air.local>
2025-06-13 12:20:41 +0800

    add header

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Act1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowHeader();
+
+            Console.WriteLine("Feature 1: Base setup with header only.");
+        }
+
+        static void ShowHeader()
+        {
+            Console.WriteLine("===== Student Grading System =====\n");
+        }
+    }
+}
